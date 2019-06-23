4683c25fd1a22cdbeb3803917b2db163392ae8e5
tatsunori <tatsunoripai@gmail.com>
2019-06-23 03:46:12 -0300

    finalizado

--- Functions.py
+++ Functions.py
@@ -1,41 +1,88 @@
-import timeit
-
 import numpy as np
 import cv2
+import timeit
 
-maskHeight = 3
-maskWidth = 3
-mask = np.ones((maskWidth, maskHeight))
-
-def correlationType1(args):
+def basicCorrelation(args, mask, maskDimension):
+    startTime = timeit.default_timer()
 
     image = cv2.imread(args["image"])
-    imageResult = cv2.imread(args["image"])
     (height, width) = image.shape[:2]
 
-    for h in range (height):
-        for w in range (width):
-            imageResult[h][w] = applyMaskForPixel(image, h, w)
+    imageResult = np.float64(image)
+    for h in range(height):
+        for w in range(width):
+            imageResult[h][w] = applyMaskForPixel(image, h, w, mask, maskDimension)
+
+    imageResult = np.uint8(normalize(imageResult))
+
+    endTime = timeit.default_timer()
+    print("Tempo do algoritmo basico de correlacao: ", endTime - startTime)
+
+    showImages("Normal Correlation", [image, imageResult])
+
+def applyMaskForPixel(img, x, y, mask, maskDimension):
+    neighborhoodIntensity = 0
+    posX = x - 1
+    posY = y - 1
+
+    for i in range(maskDimension):
+        for j in range(maskDimension):
+            try:
+                neighborhoodIntensity += img[posX + i][posY + j] * mask[i][j]
+            except:
+                neighborhoodIntensity += 0
+
+    return neighborhoodIntensity / (maskDimension * maskDimension)
 
 
-    cv2.imshow("image", np.hstack([image, imageResult]))
+def fastCorrelation(args, mask, maskDimension):
+    startTime = timeit.default_timer()
+    BLACK = [0, 0, 0]
+    border = 1
+    image = cv2.imread(args["image"])
+    (height, width) = image.shape[:2]
+
+    images = []
+    weights = createMask(mask, maskDimension)
+
+    imageBordered = cv2.copyMakeBorder(image, border, border, border, border, cv2.BORDER_CONSTANT, None, value=BLACK)
+    imageBordered = np.float64(imageBordered)
+
+    for i in range(len(weights)):
+        imageAux = np.copy(imageBordered)
+        imageAux = np.roll(imageAux, weights[i][0][0], axis=0)
+        imageAux = np.roll(imageAux, weights[i][0][1], axis=1)
+        np.multiply(imageAux, weights[i][1], out=imageAux, casting='unsafe')
+        images.append(imageAux)
+
+    result = np.sum(images, axis = 0)
+    result = np.uint8(normalize(result))
+    result = result[1:height+1, 1:width+1]
+
+    endTime = timeit.default_timer()
+    print("Tempo do algoritmo rapido de correlacao: ", endTime - startTime)
+
+    showImages("Fast Correlation", [image, result])
+
+def createMask(mask, maskDimension):
+    weights = []
+    for i in range(maskDimension):
+        for j in range(maskDimension):
+            x = -abs(i) + 1
+            y = -abs(j) + 1
+            coordinate = [x, y]
+            weights.append([coordinate, mask[i][j]])
+    return weights
+
+def normalize(image):
+    image += np.abs(np.amin(image))
+    image *= (1.0 / np.amax(image))
+    image *= 255.0
+    return image
+
+def showImages(name, images):
+    cv2.imshow(name, np.hstack(images))
     cv2.waitKey(0)
 
-def applyMaskForPixel(img, x, y):
 
-    try:
-        neighborhoodIntensity = img[x - 1][y + 1] * mask[0][0]
-        neighborhoodIntensity += img[x - 1][y] * mask[1][0]
-        neighborhoodIntensity += img[x - 1][y - 1] * mask[2][0]
 
-        neighborhoodIntensity += img[x][y + 1] * mask[0][1]
-        neighborhoodIntensity += img[x][y] * mask[1][1]
-        neighborhoodIntensity += img[x][y - 1] * mask[2][1]
-
-        neighborhoodIntensity += img[x + 1][y + 1] * mask[0][2]
-        neighborhoodIntensity += img[x + 1][y] * mask[1][2]
-        neighborhoodIntensity += img[x + 1][y - 1] * mask[2][2]
-    except:
-        neighborhoodIntensity = img[x, y]
-
-    return neighborhoodIntensity / (maskWidth * maskHeight)
--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import Functions as F
 
 # argumentos do programa
@@ -7,4 +8,15 @@ ap.add_argument("-i", "--image", required=True, help="Path to the image")
 
 args = vars(ap.parse_args())
 
-F.correlationType1(args)
+maskDimension = int(input("Digite a dimensão da matriz: "))
+mask = np.zeros(maskDimension * maskDimension)
+mask = mask.reshape((maskDimension, maskDimension))
+
+for i in range(maskDimension):
+    for j in range(maskDimension):
+        mask[i][j] = int(input("Digite o elemento [%d][%d]: " % (i, j)))
+
+print("Aguarde")
+
+F.basicCorrelation(args, mask, maskDimension)
+F.fastCorrelation(args, mask, maskDimension)
